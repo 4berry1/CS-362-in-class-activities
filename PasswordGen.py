7864ac0d7ae1e6ffb53027687c6fec8dab0f4a30
Jordan <4berry1@gmail.com>
2021-04-15 17:46:37 -0700

    password gen fully functioning

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,7 +1,10 @@
 import random
 
-passlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
+passlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
 
 def passGen(a):
+    password = ""
     for i in range (0, a):
-        print(rand)
+        key = random.randrange(0,35,1)
+        password += passlist[key]
+    print(password)
