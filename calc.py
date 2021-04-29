f1f56263989753bc2e960347e250bc6b83f9821a
Jordan <4berry1@gmail.com>
2021-04-29 16:50:27 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,22 @@
-def calc(a,b):
-    add = a + b
-    diff = a - b
-    mult = a * b
-    devide = a / b
+def main():
+    x = int(input("enter 1st value: "))
+    y = int(input("enter 2nd value: "))
+    mode = int(input(" 1 - add \n 2 - sub \n 3 - divide \n 4 - multiply \n :"))
     
-    print(add, diff, mult, devide)
+    if (mode == 1):
+        ans = x + y
+        print (ans)
+    if (mode == 2):
+        ans = x - y
+        print (ans)
+    if (mode == 3):
+        ans = x / y
+        print (ans)
+    if (mode == 4):
+        ans = x * y
+        print (ans)
 
-    sumall = add + diff + mult + devide
+    
+main()
 
-    print(sumall)
+
