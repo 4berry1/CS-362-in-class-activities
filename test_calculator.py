7347ba4ff64b634e4677949804c74afdd8e024d2
Jordan <4berry1@gmail.com>
2021-06-07 21:32:33 -0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,11 +4,9 @@ import calculator
 
 class TestCalculator:
 
-
     def test_add(self):
         assert 5 == calculator.add(1, 4)
 
 
     def test_subtract(self):
         assert 2 == calculator.sub(4, 2)
-        
